a11d8107f066de70daced03675acb01650578921
Darryl Wagoner <darryl@wagoner.me>
2024-04-11 16:48:59 -0500

    Check point before adding http service

--- OneStream/Pages/Home.razor.cs
+++ OneStream/Pages/Home.razor.cs
@@ -5,8 +5,18 @@ public partial class Home : ComponentBase
     [Inject] protected IMatToaster Toaster { get; set; }
     private string Callsign { get; set; } = string.Empty;
     private string Notes { get; set; } = string.Empty;
-    public async Task OnClick(MouseEventArgs e)
+    public async Task LookupClick(MouseEventArgs e)
     {
         Toaster.Add("Toaster test " + Callsign, MatToastType.Danger, "title");
+    }
+
+    private async Task ReplaceNotesClick(MouseEventArgs arg)
+    {
+        throw new NotImplementedException();
+    }
+
+    private async Task AddNotesClick()
+    {
+        throw new NotImplementedException();
     }
 }
