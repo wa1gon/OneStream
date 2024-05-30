676817cdc66f27be886323d8a8ade3b3a1f13367
Darryl Wagoner <darryl@wagoner.me>
2024-05-30 13:56:33 -0500

    warning fixes

--- CallsignAPI/Controllers/CallSignLookupController.cs
+++ CallsignAPI/Controllers/CallSignLookupController.cs
@@ -2,32 +2,32 @@
 
 [ApiController]
 [Route("[controller]")]
-public class CallSignLookupController: ControllerBase
+public class CallSignLookupController : ControllerBase
 {
     private readonly IRepoService _repo;
 
-    public CallSignLookupController( IRepoService repo)
+    public CallSignLookupController(IRepoService repo)
     {
         _repo = repo;
     }
     [HttpGet(Name = "Lookup")]
     public async Task<ActionResult<CallsignInfo>> Get([FromQuery] string callsign)
     {
-        Task.Delay(1000);  
+        await Task.Delay(1000);
         var result = await _repo.GetCallsignDetailsAsync(callsign);
         return result;
     }
     [HttpPost(Name = "addNote")]
-    public async  Task<CallsignInfo> Post( [FromBody] CallUpdateDTO callUpdate)
+    public async Task<CallsignInfo> Post([FromBody] CallUpdateDTO callUpdate)
     {
-        Task.Delay(1500);
+        await Task.Delay(1500);
         var result = await _repo.ReplaceNoteInCache(callUpdate);
         return result;
     }
     [HttpPut(Name = "updateNote")]
     public async Task<CallsignInfo> Put([FromBody] CallUpdateDTO callUpdate)
     {
-        Task.Delay(2000);
+        await Task.Delay(2000);
         var result = await _repo.AddNoteInCache(callUpdate);
         return result;
     }
--- CallsignAPI/GlobalUsing.cs
+++ CallsignAPI/GlobalUsing.cs
@@ -1,8 +1,7 @@
-﻿global using CallsignAPI.Services;
+﻿global using CallsignAPI.Abstractions;
+global using CallsignAPI.Services;
+global using CallSignCommon.ExtensionMethods;
 global using CallSignCommon.Models;
-global using CallsignAPI.Abstractions;
 global using Microsoft.AspNetCore.Mvc;
 global using System.Text.Json;
-global using CallSignCommon.ExtensionMethods;
 global using System.Text.RegularExpressions;
-global using CallSignCommon.ExtensionMethods;
